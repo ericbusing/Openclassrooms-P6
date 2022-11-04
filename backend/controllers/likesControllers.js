fad7450c472a4217c8f63b5a8cf03e55d6529aaf
eric <busing.eric@gmail.com>
2022-11-04 12:50:30 +0100

    commit

--- backend/controllers/likesControllers.js
+++ backend/controllers/likesControllers.js
@@ -1,59 +1,63 @@
 // Importation du modele de sauce.
-const Sauce = require("../models/saucesModel")
+const Sauce = require("../models/saucesModel");
 
 exports.likeSauce = (req, res) => {
-    if (req.body.like === 1) {
-        // Mise a jour de la sauce dans la bdd, et de son array.
-        Sauce.updateOne(
+  if (req.body.like === 1) {
+    // Mise a jour de la sauce dans la bdd, et de son array.
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        // Utilisation de l'operateur $inc.
+        $inc: { likes: 1 },
+        $push: { usersLiked: req.body.userId },
+      },
+      console.log("wesh", req.body)
+    )
+
+      .then(() => res.status(200).json({ message: "+1 like !" }))
+      .catch((error) => res.status(400).json({ error }));
+  } else if (req.body.like === -1) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      {
+        // (req.body.like++) * -1 permet au dislike d'appliquer la bonne decrementation. Exemple:(2)*-1=1.
+        $inc: { dislikes: req.body.like++ * -1 },
+        // Utilisation de l'operateur $push.
+        $push: { usersDisliked: req.body.userId },
+      }
+    )
+
+      .then(() => res.status(200).json({ message: "+1 dislike !" }))
+      .catch((error) => res.status(400).json({ error }));
+  } else {
+    // Recuperation de la sauce dans la bdd.
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        // Utilisation de la methode includes().
+        if (sauce.usersLiked.includes(req.body.userId)) {
+          Sauce.updateOne(
             { _id: req.params.id },
             {
-                // Utilisation de l'operateur $inc.
-                $inc: { likes: 1 },
-                $push: { usersLiked: req.body.userId }
-            })
+              // Utilisation de l'operateur $pull.
+              $pull: { usersLiked: req.body.userId },
+              $inc: { likes: -1 },
+            }
+          )
 
-            .then(() => res.status(200).json({ message: '+1 like !' }))
-            .catch(error => res.status(400).json({ error }))
-
-    } else if (req.body.like === -1) {
-        Sauce.updateOne(
+            .then(() => res.status(200).json({ message: "like -1 !" }))
+            .catch((error) => res.status(400).json({ error }));
+        } else if (sauce.usersDisliked.includes(req.body.userId)) {
+          Sauce.updateOne(
             { _id: req.params.id },
             {
-                // (req.body.like++) * -1 permet au dislike d'appliquer la bonne decrementation. Exemple:(2)*-1=1.
-                $inc: { dislikes: (req.body.like++) * -1 },
-                // Utilisation de l'operateur $push.
-                $push: { usersDisliked: req.body.userId }
-            })
-
-            .then(() => res.status(200).json({ message: '+1 dislike !' }))
-            .catch(error => res.status(400).json({ error }))
-
-    } else {
-        // Recuperation de la sauce dans la bdd.
-        Sauce.findOne({ _id: req.params.id })
-            .then(sauce => {
-                // Utilisation de la methode includes().
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    Sauce.updateOne({ _id: req.params.id },
-                        {
-                            // Utilisation de l'operateur $pull.
-                            $pull: { usersLiked: req.body.userId },
-                            $inc: { likes: -1 }
-                        })
-
-                        .then(() => res.status(200).json({ message: 'like -1 !' }))
-                        .catch(error => res.status(400).json({ error }))
-
-                } else if (sauce.usersDisliked.includes(req.body.userId)) {
-                    Sauce.updateOne({ _id: req.params.id },
-                        {
-                            $pull: { usersDisliked: req.body.userId },
-                            $inc: { dislikes: -1 }
-                        })
-                        .then(() => res.status(200).json({ message: 'Dislike -1 !' }))
-                        .catch(error => res.status(400).json({ error }))
-                }
-            })
-            .catch(error => res.status(400).json({ error }))
-    }
-}
+              $pull: { usersDisliked: req.body.userId },
+              $inc: { dislikes: -1 },
+            }
+          )
+            .then(() => res.status(200).json({ message: "Dislike -1 !" }))
+            .catch((error) => res.status(400).json({ error }));
+        }
+      })
+      .catch((error) => res.status(400).json({ error }));
+  }
+};
